24358997cbc958ffd04bcbdeded9167f2bb9bb89
anastasiadergaeva <adergaeva@gmail.com>
2023-11-29 14:41:22 +0300

    add function closing burger menu after click

--- assets/script/responsive.js
+++ assets/script/responsive.js
@@ -5,6 +5,13 @@ function openResponsiveMenu() {
     headerBurgerElement.classList.toggle('active');
     headerMenuElement.classList.toggle('active');
     bodyElement.classList.toggle('lock');
+    document.querySelectorAll('.header__list a').forEach(function (element) {
+        element.addEventListener("click", function () {
+            headerBurgerElement.classList.remove('active');
+            headerMenuElement.classList.remove('active');
+            bodyElement.classList.remove('lock');
+        });
+    });
 }
 
 document.querySelector('.header__burger').onclick = openResponsiveMenu;
